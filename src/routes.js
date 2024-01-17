dcea4a5515def0e1000dde109531cca6289a3e2d
Alisson Delporto <timoalisson@gmail.com>
2024-01-16 22:03:43 -0300

    Finish

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
+import Favoritos from "./pages/Favoritos";
 
 import Erro from "./pages/Erro";
 
@@ -13,6 +14,7 @@ function Routesapp(){
             <Routes>
                <Route path="/" element={ <Home/>} />
                <Route path="/filme/:id" element={ <Filme/>} />
+               <Route path="/favoritos" element={ <Favoritos/>} />
 
 
                <Route path="*" element={ <Erro/> } />
